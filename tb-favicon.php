c7e113caaa7c9788d80ae4e682b256a166f3cefb
Jason Bobich <jasonbobich@gmail.com>
2015-09-10 10:43:09 -0800

    Code cleanup & very minor security fixes

--- tb-favicon.php
+++ tb-favicon.php
@@ -38,7 +38,7 @@ define( 'TB_FAVICON_PLUGIN_URI', plugins_url( '' , __FILE__ ) );
 function themeblvd_favicon_init() {
 
 	// Check to make sure Theme Blvd Framework 2.2+ is running
-	if( ! defined( 'TB_FRAMEWORK_VERSION' ) || version_compare( TB_FRAMEWORK_VERSION, '2.1.0', '<' ) ) {
+	if ( ! defined( 'TB_FRAMEWORK_VERSION' ) || version_compare( TB_FRAMEWORK_VERSION, '2.1.0', '<' ) ) {
 		add_action( 'admin_notices', 'themeblvd_favicon_warning' );
 		return;
 	}
@@ -47,18 +47,18 @@ function themeblvd_favicon_init() {
 	themeblvd_add_option_tab( 'config', __('Configuration', 'theme-blvd-favicon') );
 
 	// Setup option params
-	$name = __('Favicons', 'theme-blvd-favicon');
-	$description = __('A favicon generally appears at the top of the web browser next to the title or URL of your website for your online visitors. The favicon also gets associated with your website when users bookmark it. In addition to the standard favicon, you can also configure the Apple touch icons that users see when bookmarking your website from various Apple devices.<br><br><strong>These are all completely optional; any items you leave blank in this section will simply not display on your website.</strong>', 'theme-blvd-favicon');
+	$description = __('A favicon generally appears at the top of the web browser next to the title or URL of your website for your online visitors. The favicon also gets associated with your website when users bookmark it. In addition to the standard favicon, you can also configure the Apple touch icons that users see when bookmarking your website from various Apple devices.', 'theme-blvd-favicon').'<br><br><strong>'.__('These are all completely optional; any items you leave blank in this section will simply not display on your website.', 'theme-blvd-favicon').'</strong>';
+
 	$options = apply_filters( 'themeblvd_favicon_options', array(
 		'favicon' => array(
 			'name' 		=> __('Standard Favicon (16x16)', 'theme-blvd-favicon'),
-			'desc' 		=> __('Insert the URL to the .ico file or PNG image you\'d like to use as your website\'s favicon.<br><br>For help generating a standard favicon ICO file, visit <a href="http://www.favicon.cc/" target="_blank">favicon.cc</a>.', 'theme-blvd-favicon'),
+			'desc' 		=> __('Insert the URL to the .ico file or PNG image you\'d like to use as your website\'s favicon.', 'theme-blvd-favicon').'<br><br>'.__('For help generating a standard favicon ICO file, visit <a href="http://www.favicon.cc/" target="_blank">favicon.cc</a>.', 'theme-blvd-favicon'),
 			'id' 		=> 'favicon',
 			'type' 		=> 'upload'
 		),
 		'apple_touch_57x57' => array(
 			'name' 		=> __('Standard Apple Touch Icon (57x57)', 'theme-blvd-favicon'),
-			'desc' 		=> __('Insert the URL to the PNG image file you\'d like to use as your website\'s Apple touch icon on standard apple devices.<br><br>This is more of the "fallback" for older Apple devices.', 'theme-blvd-favicon'),
+			'desc' 		=> __('Insert the URL to the PNG image file you\'d like to use as your website\'s Apple touch icon on standard apple devices.', 'theme-blvd-favicon').'<br><br>'.__('This is more of the "fallback" for older Apple devices.', 'theme-blvd-favicon'),
 			'id' 		=> 'apple_touch_57x57',
 			'type' 		=> 'upload'
 		),
@@ -83,7 +83,7 @@ function themeblvd_favicon_init() {
 	));
 
 	// Add option section
-	themeblvd_add_option_section( 'config', 'favicons', $name, $description, $options );
+	themeblvd_add_option_section( 'config', 'favicons', __('Favicons', 'theme-blvd-favicon'), $description, $options );
 
 	// Hook our output to wp_head
 	add_action( 'wp_head', 'themeblvd_favicon' );
@@ -126,24 +126,34 @@ function themeblvd_favicon() {
 
 	// Standard favicon
 	$icon_16x16 = themeblvd_get_option('favicon');
-	if( $icon_16x16 )
+
+	if ( $icon_16x16 ) {
 		echo '<link rel="shortcut icon" href="'.$icon_16x16.'" />'."\n";
+	}
 
 	// Apple touch icons
 	$icon_144x144 = themeblvd_get_option('apple_touch_144x144');
-	if( $icon_144x144 )
+
+	if ( $icon_144x144 ) {
 		echo '<link rel="apple-touch-icon-precomposed" sizes="144x144" href="'.$icon_144x144.'" />'."\n";
+	}
 
 	$icon_114x114 = themeblvd_get_option('apple_touch_114x114');
-	if( $icon_114x114 )
+
+	if ( $icon_114x114 ) {
 		echo '<link rel="apple-touch-icon-precomposed" sizes="114x114" href="'.$icon_114x114.'" />'."\n";
+	}
 
 	$icon_72x72 = themeblvd_get_option('apple_touch_72x72');
-	if( $icon_72x72 )
+
+	if ( $icon_72x72 ) {
 		echo '<link rel="apple-touch-icon-precomposed" sizes="72x72" href="'.$icon_72x72.'" />'."\n";
+	}
 
 	$icon_57x57 = themeblvd_get_option('apple_touch_57x57');
-	if( $icon_57x57 )
+
+	if ( $icon_57x57 ) {
 		echo '<link rel="apple-touch-icon-precomposed" href="'.$icon_57x57.'" />'."\n";
+	}
 
 }
